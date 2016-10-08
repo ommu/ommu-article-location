49f4ee2f38f2cc4224c3f22886e0d96053a8e4fd
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-10-08 07:42:00 +0700

    merge with other projects

--- protected/ommu/Ommu.php
+++ protected/ommu/Ommu.php
@@ -21,6 +21,8 @@
  *
  *----------------------------------------------------------------------------------------------------------
  */
+Yii::import('application.components.plugin.Spyc');
+define('DS', DIRECTORY_SEPARATOR);
 
 class Ommu extends CApplicationComponent
 {
@@ -39,6 +41,18 @@ class Ommu extends CApplicationComponent
 			$theme = trim($_GET['theme']);
 		}
 		Yii::app()->theme = $theme;
+		
+		/**
+		 * controllerMap
+		 */
+		$themePath = Yii::getPathOfAlias('webroot.themes.'.$theme).DS.$theme.'.yaml';
+		$arrayThemeSpyc = Spyc::YAMLLoad($themePath);
+		$controllerSpyc = $arrayThemeSpyc['controller'];
+		if(!empty($controllerSpyc)) {
+			foreach($controllerSpyc as $key => $val)
+				$controllerMap[$key] = 'webroot.themes.'.$theme.'.controllers.'.$val;
+			Yii::app()->controllerMap = $controllerMap;
+		}
 
 		/**
 		 * set url manager
@@ -151,10 +165,14 @@ class Ommu extends CApplicationComponent
 			'og:title'=>'MY_WEBSITE_NAME',
 			'og:description'=>'MY_WEBSITE_DESCRIPTION',
 			'og:image'=>$metaImages,
-			'og:site_name'=>$meta->facebook_sitename,
-			'og:see_also'=>$meta->facebook_see_also,
-			'fb:admins'=>$meta->facebook_admins,
 		);
+		if($meta->facebook_sitename != '')
+			$arrayFacebookGlobal['og:site_name'] = $meta->facebook_sitename;
+		if($meta->facebook_see_also != '')
+			$arrayFacebookGlobal['og:see_also'] = $meta->facebook_see_also;
+		if($meta->facebook_admins != '')
+			$arrayFacebookGlobal['fb:admins'] = $meta->facebook_admins;
+		
 		if($meta->facebook_type == 1) {
 			$arrayFacebook = array(
 				'profile:first_name'=>$meta->facebook_profile_firstname,
@@ -180,23 +198,41 @@ class Ommu extends CApplicationComponent
 			);
 		} else {
 			$cardType = 'app';
-			$arrayTwitter = array(
-				'twitter:app:id:iphone'=>$meta->twitter_iphone_id,
-				'twitter:app:url:iphone'=>$meta->twitter_iphone_url,
-				'twitter:app:name:ipad'=>$meta->twitter_ipad_name,
-				'twitter:app:url:ipad'=>$meta->twitter_ipad_url,
-				'twitter:app:id:googleplay'=>$meta->twitter_googleplay_id,
-				'twitter:app:url:googleplay'=>$meta->twitter_googleplay_url,
-			);
+			if($meta->twitter_country != '')
+				$arrayTwitter['twitter:app:country'] = $meta->twitter_country;
+			if($meta->twitter_iphone_name != '')
+				$arrayTwitter['twitter:app:name:iphone'] = $meta->twitter_iphone_name;
+			if($meta->twitter_iphone_id != '')
+				$arrayTwitter['twitter:app:id:iphone'] = $meta->twitter_iphone_id;
+			if($meta->twitter_iphone_url != '')
+				$arrayTwitter['twitter:app:url:iphone'] = $meta->twitter_iphone_url;
+			if($meta->twitter_ipad_name != '')
+				$arrayTwitter['twitter:app:name:ipad'] = $meta->twitter_ipad_name;
+			if($meta->twitter_ipad_id != '')
+				$arrayTwitter['twitter:app:id:ipad'] = $meta->twitter_ipad_id;
+			if($meta->twitter_ipad_url != '')
+				$arrayTwitter['twitter:app:url:ipad'] = $meta->twitter_ipad_url;
+			if($meta->twitter_googleplay_name != '')
+				$arrayTwitter['twitter:app:name:googleplay'] = $meta->twitter_googleplay_name;
+			if($meta->twitter_googleplay_id != '')
+				$arrayTwitter['twitter:app:id:googleplay'] = $meta->twitter_googleplay_id;
+			if($meta->twitter_googleplay_url != '')
+				$arrayTwitter['twitter:app:url:googleplay'] = $meta->twitter_googleplay_url;
+			
+			if(empty($arrayTwitter))
+				$arrayTwitter = array();
 		}
 		$arrayTwitterGlobal = array(
 			'twitter:card'=>$cardType,
-			'twitter:site'=>$meta->twitter_card != 4 ? $meta->twitter_site : '',
-			'twitter:creator'=>$meta->twitter_card != 4 ? $meta->twitter_creator : '',
+			'twitter:site'=>$meta->twitter_site,
 			'twitter:title'=>'MY_WEBSITE_NAME',
 			'twitter:description'=>'MY_WEBSITE_DESCRIPTION',
-			'twitter:image:src'=>$metaImages,
+			'twitter:image'=>$metaImages,
 		);
+		if(in_array($meta->twitter_card, array(2)))
+			$arrayTwitterGlobal['twitter:creator'] = $meta->twitter_creator;
+		if($meta->meta_image_alt != '' && in_array($meta->meta_image_alt, array(1,2)))
+			$arrayTwitterGlobal['twitter:image:alt'] = $meta->meta_image_alt;
 		
 		/**
 		 * Registe Meta Tags
