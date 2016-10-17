9fe4edc5515a09dceef066c0d40e439b5f7c9b7d
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-10-18 02:32:41 +0700

    article: location crud

---
+++ protected/modules/article/views/location/admin/admin_add.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Article Locations (article-locations)
+ * @var $this AdminController
+ * @var $model ArticleLocations
+ * @var $form CActiveForm
+ * version: 0.0.1
+ *
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @copyright Copyright (c) 2016 Ommu Platform (ommu.co)
+ * @created date 18 October 2016, 02:29 WIB
+ * @link http://company.ommu.co
+ * @contect (+62)856-299-4114
+ *
+ */
+
+	$this->breadcrumbs=array(
+		'Article Locations'=>array('manage'),
+		'Create',
+	);
+?>
+
+<div class="form">
+	<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+</div>
---
+++ protected/modules/article/views/location/admin/front_index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Article Locations (article-locations)
+ * @var $this AdminController
+ * @var $model ArticleLocations
+ * @var $dataProvider CActiveDataProvider
+ * version: 0.0.1
+ *
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @copyright Copyright (c) 2016 Ommu Platform (ommu.co)
+ * @created date 18 October 2016, 02:29 WIB
+ * @link http://company.ommu.co
+ * @contect (+62)856-299-4114
+ *
+ */
+
+	$this->breadcrumbs=array(
+		'Article Locations',
+	);
+?>
+
+<?php $this->widget('application.components.system.FListView', array(
+	'dataProvider'=>$dataProvider,
+	'itemView'=>'_view',
+	'pager' => array(
+		'header' => '',
+	), 
+	'summaryText' => '',
+	'itemsCssClass' => 'items clearfix',
+	'pagerCssClass'=>'pager clearfix',
+)); ?>
