b8c8f75e6d799cfd885140db07aab466d9193bba
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-10-18 02:33:21 +0700

    article: location user crud

---
+++ protected/modules/article/views/location/user/_form.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * Article Location Users (article-location-user)
+ * @var $this UserController
+ * @var $model ArticleLocationUser
+ * @var $form CActiveForm
+ * version: 0.0.1
+ *
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @copyright Copyright (c) 2016 Ommu Platform (ommu.co)
+ * @created date 18 October 2016, 02:29 WIB
+ * @link http://company.ommu.co
+ * @contect (+62)856-299-4114
+ *
+ */
+?>
+
+<?php $form=$this->beginWidget('application.components.system.OActiveForm', array(
+	'id'=>'article-location-user-form',
+	'enableAjaxValidation'=>true,
+	//'htmlOptions' => array('enctype' => 'multipart/form-data')
+)); ?>
+
+<?php //begin.Messages ?>
+<div id="ajax-message">
+	<?php echo $form->errorSummary($model); ?>
+</div>
+<?php //begin.Messages ?>
+
+<fieldset>
+
+	<div class="clearfix">
+		<?php echo $form->labelEx($model,'id'); ?>
+		<div class="desc">
+			<?php echo $form->textField($model,'id'); ?>
+			<?php echo $form->error($model,'id'); ?>
+			<?php /*<div class="small-px silent"></div>*/?>
+		</div>
+	</div>
+
+	<div class="clearfix publish">
+		<?php echo $form->labelEx($model,'status'); ?>
+		<div class="desc">
+			<?php echo $form->checkBox($model,'status'); ?>
+			<?php echo $form->labelEx($model,'status'); ?>
+			<?php echo $form->error($model,'status'); ?>
+			<?php /*<div class="small-px silent"></div>*/?>
+		</div>
+	</div>
+
+	<div class="clearfix">
+		<?php echo $form->labelEx($model,'location_id'); ?>
+		<div class="desc">
+			<?php echo $form->textField($model,'location_id'); ?>
+			<?php echo $form->error($model,'location_id'); ?>
+			<?php /*<div class="small-px silent"></div>*/?>
+		</div>
+	</div>
+
+	<div class="clearfix">
+		<?php echo $form->labelEx($model,'creation_date'); ?>
+		<div class="desc">
+			<?php echo $form->textField($model,'creation_date'); ?>
+			<?php echo $form->error($model,'creation_date'); ?>
+			<?php /*<div class="small-px silent"></div>*/?>
+		</div>
+	</div>
+
+	<div class="clearfix">
+		<?php echo $form->labelEx($model,'creation_id'); ?>
+		<div class="desc">
+			<?php echo $form->textField($model,'creation_id',array('size'=>11,'maxlength'=>11)); ?>
+			<?php echo $form->error($model,'creation_id'); ?>
+			<?php /*<div class="small-px silent"></div>*/?>
+		</div>
+	</div>
+
+	<div class="clearfix">
+		<?php echo $form->labelEx($model,'modified_date'); ?>
+		<div class="desc">
+			<?php echo $form->textField($model,'modified_date'); ?>
+			<?php echo $form->error($model,'modified_date'); ?>
+			<?php /*<div class="small-px silent"></div>*/?>
+		</div>
+	</div>
+
+	<div class="clearfix">
+		<?php echo $form->labelEx($model,'modified_id'); ?>
+		<div class="desc">
+			<?php echo $form->textField($model,'modified_id',array('size'=>11,'maxlength'=>11)); ?>
+			<?php echo $form->error($model,'modified_id'); ?>
+			<?php /*<div class="small-px silent"></div>*/?>
+		</div>
+	</div>
+
+	<div class="submit clearfix">
+		<label>&nbsp;</label>
+		<div class="desc">
+			<?php echo CHtml::submitButton($model->isNewRecord ? Yii::t('phrase', 'Create') : Yii::t('phrase', 'Save'), array('onclick' => 'setEnableSave()')); ?>
+		</div>
+	</div>
+
+</fieldset>
+<?php /*
+<div class="dialog-content">
+</div>
+<div class="dialog-submit">
+	<?php echo CHtml::submitButton($model->isNewRecord ? Yii::t('phrase', 'Create') : Yii::t('phrase', 'Save') ,array('onclick' => 'setEnableSave()')); ?>
+	<?php echo CHtml::button(Yii::t('phrase', 'Cancel'), array('id'=>'closed')); ?>
+</div>
+*/?>
+<?php $this->endWidget(); ?>
+
+
