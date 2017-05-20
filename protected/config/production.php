bfbd33d8cb146bb9de2099f91f326dabe723f689
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-05-20 16:15:08 +0700

    commit

--- protected/config/production.php
+++ protected/config/production.php
@@ -14,7 +14,7 @@
  */
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'CoE',
+	'name'=>'CoE - Portal Center Of Excellence',
 
 	// Language setting
 	'sourceLanguage' => '00',
