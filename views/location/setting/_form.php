b20a6595d1b1e1d8bae8de87fa3e417be0e82807
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-07-09 12:23:07 +0700

    change yii components

--- views/location/setting/_form.php
+++ views/location/setting/_form.php
@@ -27,7 +27,7 @@ EOP;
 	$cs->registerScript('js', $js, CClientScript::POS_END);
 ?>
 
-<?php $form=$this->beginWidget('application.libraries.core.components.system.OActiveForm', array(
+<?php $form=$this->beginWidget('application.libraries.yii-traits.system.OActiveForm', array(
 	'id'=>'article-location-setting-form',
 	'enableAjaxValidation'=>true,
 	//'htmlOptions' => array('enctype' => 'multipart/form-data')
--- views/location/tag/_form.php
+++ views/location/tag/_form.php
@@ -14,7 +14,7 @@
  */
 ?>
 
-<?php $form=$this->beginWidget('application.libraries.core.components.system.OActiveForm', array(
+<?php $form=$this->beginWidget('application.libraries.yii-traits.system.OActiveForm', array(
 	'id'=>'article-location-tag-form',
 	'enableAjaxValidation'=>true,
 	//'htmlOptions' => array('enctype' => 'multipart/form-data')
